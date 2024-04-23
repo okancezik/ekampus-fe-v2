f072ef8bc35035ae436f9bc93b1360943f8f4879
Okan Cezik <okacezik@gmail.com>
2024-04-23 15:56:20 +0300

    side menu

--- src/layouts/base-layout/base-layout.tsx
+++ src/layouts/base-layout/base-layout.tsx
@@ -1,20 +1,21 @@
-import { Col, Row } from 'antd'
 import React from 'react'
-import SideMenu from '../side-menu/side-menu'
+import './base-layout.css'
+import { Col, Row } from 'antd'
 import TimelineLayout from '../timeline-layout/timeline-layout'
+import Side from '../side-menu/side-menu'
 
 const BaseLayout = () => {
-  return (
-    <Row>
-        <Col span={6}>
-        <SideMenu/>
-        </Col>
-        <Col span={12}>
-        <TimelineLayout/>
-        </Col>
-        <Col span={6}>3</Col>
-    </Row>
-  )
+    return (
+        <Row className='base-container'>
+            <Col span={6}>
+                <Side />
+            </Col>
+            <Col span={12}>
+                <TimelineLayout />
+            </Col>
+            <Col span={6}>3</Col>
+        </Row>
+    )
 }
 
 export default BaseLayout
--- src/layouts/side-menu/side-menu.tsx
+++ src/layouts/side-menu/side-menu.tsx
@@ -1,35 +1,35 @@
 import React from 'react'
-import './side-menu-module.css'
+import './side-menu.css';
+import logo from '../../assets/ekampus.png';
 import { HomeOutlined, CompassOutlined, SearchOutlined } from '@ant-design/icons';
 import EKAvatar from '../../components/ek-avatar/ek-avatar';
-import ekampus from "../../assets/ekampus.png";
 
 const SideMenu = () => {
     return (
-        <div className='side_container'>
-            <img src={ekampus} alt='logo' className='logo' />
-            <div className='side__buttons'>
-                <button className='sidenav__button'>
-                    <HomeOutlined style={{ fontSize: '1.5rem' }} />
-                    <span>Home</span>
-                </button>
-                <button className='sidenav__button'>
-                    <CompassOutlined style={{ fontSize: '1.5rem' }} />
-                    <span>Explore</span>
-                </button>
-                <button className='sidenav__button'>
-                    <SearchOutlined style={{ fontSize: '1.5rem' }} />
-                    <span>Search</span>
-                </button>
-                <button className='sidenav__button'>
-                    <EKAvatar />
-                    <span>Profile</span>
-                </button>
+        <div className='side-container'>
+            <div className='header-container'>
+                <img src={logo} alt='logo' className='logo' />
+                <div className='options-container'>
+                    <div className='option'>
+                        <HomeOutlined />
+                        <span>Ana sayfa</span>
+                    </div>
+                    <div className='option'>
+                        <CompassOutlined />
+                        <span>Keşfet</span>
+                    </div>
+                    <div className='option'>
+                        <SearchOutlined />
+                        <span>Ara</span>
+                    </div>
+                    <div className='option'>
+                        <EKAvatar />
+                        <span>Profile</span>
+                    </div>
+                </div>
             </div>
-            <div className='sidenav__more'>
-                <button className='sidenav__button'>
-                    More
-                </button>
+            <div className='more-container'>
+                more
             </div>
         </div>
     )
